755979942f6168d905954b39c21fff4fdd2364c4
renga1080 <32118579+renga1080@users.noreply.github.com>
2017-09-27 21:34:37 -0400

    added file via initial upload

---
+++ src/main/java/co/ranga/vs/vsspringcloud/VsSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package co.ranga.vs.vsspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsSpringCloudApplication.class, args);
+	}
+}
---
+++ src/test/java/co/ranga/vs/vsspringcloud/VsSpringCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package co.ranga.vs.vsspringcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VsSpringCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
