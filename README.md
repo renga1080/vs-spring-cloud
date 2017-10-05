6ba48a4390f7f49a2d7117a38bb2a3848bc745d4
Rengarajan <krengarajan10@gmail.com>
2017-10-04 23:11:50 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vs-spring-cloud
+
+banner url : https://devops.datenkollektiv.de/banner.txt/index.html
+banner font : big or doom
--- config-server/src/main/java/com/ranga/vs/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/ranga/vs/config/ConfigServerApplication.java
@@ -1,5 +1,38 @@
 package com.ranga.vs.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-public class ConfigServerApplication {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+@Slf4j
+public class ConfigServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        try {
+            System.setProperty("HOST", InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return builder.sources(ConfigServerApplication.class);
+        //return super.configure(builder);
+
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "local");
+        //System.setProperty("PORT", "0");
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
