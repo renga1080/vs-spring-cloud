950e215c9f7200c7e6927a92f3973944486d24b0
Rengarajan <krengarajan10@gmail.com>
2017-10-05 23:22:07 -0400

    First commit

--- config-server/ReadMe.txt
+++ config-server/ReadMe.txt
@@ -1,8 +1,9 @@
 Service URLS:
 
-http://localhost:8181/config-server/develop/vs-user-service-sit.yml
-http://localhost:8181/config-server/develop/vs-user-service-sit.json
+http://localhost:8181/config-server/develop/vs-user-service-local.yml
+http://localhost:8181/config-server/develop/vs-user-service-local.json
 http://localhost:8181/config-server/develop/vs-user-service-sit.properties
 
 
+http://localhost:8181/config-server/vs-user-service/local/develop
 
--- eureka-server/src/main/java/com/ranga/vs/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ranga/vs/eureka/EurekaServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServerApplication extends SpringBootServletInitializer {
 
     @Override
